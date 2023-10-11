8c972de21a5eb009abdf409f048f39a8ae636bd6
Mofassel100 <smmofasselhosain.bba.ac.22@gmail.com>
2023-10-12 02:43:36 +0600

    backend setup

---
+++ src/app/routes/index.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import express from 'express'
+
+const router = express.Router()
+
+const moduleRoutes: any[] = []
+
+moduleRoutes.forEach(route => router.use(route.path, route.route))
+export default router
